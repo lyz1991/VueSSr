68cd79af3594865c0d10662fdcd676ab8a5a4d43
lyz1991 <1051500917@qq.com>
2018-11-08 23:54:00 +0800

    增加vw单位

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,19 @@
  * Created by mac on 2018/8/22.
  */
 const autoprefixer = require("autoprefixer");
-
+const pxtoviewport = require('postcss-px-to-viewport');
+const writesvg = require('postcss-write-svg')
 module.exports = () => ({
   plugins: [
     autoprefixer({
       browsers: ["last 3 versions"],
       remove: false
+    }),
+    pxtoviewport({
+      viewportWidth: 750,
+      viewportUnit: 'vw'
+    }),
+    writesvg({
     })
   ]
 });
--- src/components/lists.vue
+++ src/components/lists.vue
@@ -1,8 +1,19 @@
+<style lang="scss">
+
+  .border {
+    padding: 10px;
+    font-size: 26px;
+    text-align: center;
+    width: 90%;
+    border: 1px solid transparent;
+  }
+</style>
 <template>
   <div>
-    <p v-for="(value, key) in lists">
+    <p v-for="(value, key) in lists" class="name">
       {{key}}是: {{value}}
     </p>
+    <div class="border">没有了</div>
   </div>
 </template>
 <script>
