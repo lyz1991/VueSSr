e2f8aebef359a5b9c89454e6d0f446e2817daf24
lyz1991 <liuyunzhao@jianshu.com>
2019-09-03 17:24:12 +0800

    update vue

---
+++ src/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <slot :user="name">
+        <p>默认的: {{ name }}</p>
+      </slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Child",
+    data() {
+      return {
+        name: 'child'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/client.vue
+++ src/pages/client.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <Lists :lists="github"></Lists>
+    <v-child v-slot="data">
+      <p>父级样式:{{ data.user }}</p>
+    </v-child>
     <p @click="go">去server</p>
   </div>
 </template>
 <script>
   import $ from '../api/api-client'
   import Lists from '../components/lists'
+  import vChild from '../components/Child'
   export default {
     components: {
-      Lists
+      Lists,
+      vChild
     },
     data () {
       return {
